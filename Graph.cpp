798bb8bec954e045dd63a624187c1ad29b901e67
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2023-07-26 23:54:06 -0700

    Better progress on graph topo

--- Graph.cpp
+++ Graph.cpp
@@ -1,15 +1,40 @@
 #include "Graph.hpp"
 
+
 using namespace std;
 
 Graph::Graph() {}
 
-void Graph::topo(Value *v, vector<Value*> &list) {
+void Graph::topo(Value *v, vector<Value*> &list, EdgeList &eList) {
     for (int i=0; i<v->prev.size(); i++) {
-        topo(v->prev[i], list);
+        topo(v->prev[i], list, eList);
+        eList.push_back({v->prev[i]->label, v->label});
     }
     list.push_back(v);
 }
+
+void Graph::visualizeGraph(Value terminal) {
+    EdgeList eListTmp;
+    vector<Value*> list;
+    this->topo(&terminal, list, eListTmp);
+    EdgeList eList;
+    // for (int i=eListTmp.size(); i>=0; i--) eList.push_back(eListTmp[i]);
+    for (int i=0; i<eListTmp.size(); i++) eList.push_back(eListTmp[i]);
+    for (int i=0; i<eList.size(); i++) {
+        cout << i << ") " << eList[i].first << " -> " << eList[i].second << endl; 
+    }
+    // this->generateDotFile()
+} 
+
+void Graph::generateDotFile(const EdgeList &edges, const string &filename) {
+    ofstream dot_file(filename);
+    dot_file << "digraph DAG {\n";
+    for (const auto &edge : edges) {
+        dot_file << "    " << edge.first << " -> " << edge.second << ";\n";
+    }
+    dot_file << "}\n";
+}
+
 
 int main() {
     Value a = Value(1, "a");
@@ -21,8 +46,9 @@ int main() {
     cout << e << endl;
     vector<Value*> l;
     Graph g = Graph();
-    g.topo(&e, l);
-    for (int i=0; i<l.size(); i++) {
-        cout << i << ") " << *l[i]; 
-    }
+    // g.topo(&e, l);
+    g.visualizeGraph(e);
+    // for (int i=0; i<l.size(); i++) {
+        // cout << i << ") " << *l[i]; 
+    // }
 }
--- Graph.hpp
+++ Graph.hpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include "Value.hpp"
 
 using namespace std;
 
+typedef pair<string, string> Edge;
+typedef vector<Edge> EdgeList;
+
 class Graph {
     public: 
         Graph();
-        void topo(Value *v, vector<Value*> &list);
+        void topo(Value *v, vector<Value*> &list, EdgeList &eList);
+        void generateDotFile(const EdgeList &edges, const string &filename);
+        void visualizeGraph(Value terminal);
 };
 
