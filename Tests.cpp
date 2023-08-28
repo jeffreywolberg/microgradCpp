9e865b717590fb74cd24d3dd95cce5c7441de4ce
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2023-08-28 09:13:19 -0400

    Clean up tests

--- Tests.cpp
+++ Tests.cpp
@@ -10,7 +10,7 @@ void sanity_check() {
     // q = z.relu() + z * x
     // h = (z * z).relu()
     // y = h + q + q * x
-    // y.backward()
+    // y.forward/backward()
     Value two = Value(2.0);
     Graph g = Graph();
 
@@ -24,7 +24,7 @@ void sanity_check() {
     g.backward(q);
     assert(x->grad == -22);
     g.zeroGrad(q);
-    cout << "Q.backward() is a success" << endl;
+    cout << "Q.forward/backward() is a success" << endl;
 
     int l = 0;
     // cout << *q << endl;
@@ -37,9 +37,8 @@ void sanity_check() {
     assert(y->data == -20);
 
     g.backward(y);
-    // cout << "X's grad " << x->grad << endl; // x's grad should be 46 when doing backward on y
     assert(x->grad == 46);
-    cout << "Y.backward() is a success" << endl;
+    cout << "Y.forward/backward() is a success" << endl;
 }
 
 void test_more_ops() {
@@ -57,7 +56,7 @@ void test_more_ops() {
     // f = e**2
     // g = f / 2.0
     // g = g + 10.0 / f
-    // g.backward()
+    // g.forward/backward()
     // apt, bpt, gpt = a, b, g
 
     Value negOne = Value(-1.0);
@@ -70,22 +69,22 @@ void test_more_ops() {
 
     Value *a = new Value(-4.0);
     Value *b = new Value(2.0);
-    Value *tmpC1 = *a + *b;
-    Value *tmpC2 = *(*tmpC1 + *tmpC1) + one;
-    Value *tmpC3 = (*(*tmpC2 + one) + *tmpC2);
-    Value *c = *tmpC3 + *(negOne * *a);
+    Value *c = *a + *b;
+    c = *(*c + *c) + one;
+    c = (*(*c + one) + *c);
+    c = *c + *(negOne * *a);
     c->label = "c";
     
     graph.backward(c);
     assert(c->data == -1);
     assert(a->grad == 3);
     assert(b->grad == 4);
-    cout << "C.backward() is a success" << endl;
+    cout << "C.forward/backward() is a success" << endl;
     graph.zeroGrad(c);
 
-    Value *tmpD1 = *(*a * *b) + *b->power(3.0);
-    Value *tmpD2 = *(*(two * *tmpD1) + *tmpD1) + *((*b + *a)->relu());
-    Value *d = *(*(three * *tmpD2) + *tmpD2) + *(*b - *a)->relu();
+    Value *d = *(*a * *b) + *b->power(3.0);
+    d = *(*(two * *d) + *d) + *((*b + *a)->relu());
+    d = *(*(three * *d) + *d) + *(*b - *a)->relu();
 
     graph.backward(d);
     assert(a->grad == 23);
@@ -93,7 +92,7 @@ void test_more_ops() {
     // cout << "B grad: " << b->grad << endl;
     assert(b->grad == 97);
     assert(d->data == 6);
-    cout << "D.backward() is a success" << endl;
+    cout << "D.forward/backward() is a success" << endl;
     graph.zeroGrad(d);
 
     Value *e = *c - *d;
@@ -106,7 +105,7 @@ void test_more_ops() {
     assert(a->grad == -20);
     assert(b->grad == -93);
     assert(e->data == -7);
-    cout << "E.backward() is a success" << endl;
+    cout << "E.forward/backward() is a success" << endl;
     graph.zeroGrad(e);
 
     Value *f = e->power(2);
@@ -114,12 +113,10 @@ void test_more_ops() {
     assert(a->grad == 280);
     assert(b->grad == 1302);
     assert(f->data == 49);
-    cout << "F.backward() is a success" << endl;
+    cout << "F.forward/backward() is a success" << endl;
     graph.zeroGrad(f);
 
-    Value *tmpG1 = *f / two;
-    Value *g = *tmpG1 + *(ten / *f);
-    
+    Value *g = *(*f / two) + *(ten / *f);
     graph.backward(g);
     
     double tol = 1e-6;
@@ -127,10 +124,13 @@ void test_more_ops() {
     assert(abs(g->data - 24.70408163265306) < tol);
     assert(abs(a->grad - 138.83381924198252) < tol);
     assert(abs(g->data - 24.70408163265306) < tol);
-    cout << "G.backward() is a success" << endl;
+    cout << "G.forward/backward() is a success" << endl;
 }
 
 int main() {
     sanity_check();
+    cout << "Sanity check passes!" << endl;
     test_more_ops();
+    cout << "More operations check passes!" << endl;
+
 }
